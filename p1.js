ef7b9f2f753f7c93f4d812bd50ce07626c2b48f5
Sydney Wu <xinni.wu@softwire.com>
2022-04-21 13:55:08 +0100

    Initial commit

--- p1.js
+++ p1.js
@@ -5,11 +5,30 @@ class Account {
         this.name = name
         this.balance = balance
     }
+
+    transferIn(amount) {
+        this.balance = this.balance + amount
+    }
+
+    transferOut(amount) {
+        this.balance = this.balance - amount
+    }
+
+    showBalance() {
+        return this.balance
+    }
 }
 let a = new Account("a", 0)
 console.log(a.name)
 
+a.transferIn(100)
+a.transferOut(50)
+console.log(a.balance)
+
+b = a.showBalance()
+console.log(b)
 
 // methods:
 // in / out
 // "transfer from sb to sb", evoke 2 basic methods
+
