ae57a886ee558aedc67c269df41d21c85a97bca6
Sydney Wu <xinni.wu@softwire.com>
2022-04-21 13:27:19 +0100

    Initial comment

---
+++ p1.js
@@ -0,0 +1,15 @@
+
+// class of account
+class Account {
+    constructor(name, balance) {
+        this.name = name
+        this.balance = balance
+    }
+}
+let a = new Account("a", 0)
+console.log(a.name)
+
+
+// methods:
+// in / out
+// "transfer from sb to sb", evoke 2 basic methods
